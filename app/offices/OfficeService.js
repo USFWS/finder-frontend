4c73fb1d117aae836531f5b2f305cc75c4a09654
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:44:12 -0400

    Create service for getting offices

--- app/offices/OfficeService.js
+++ app/offices/OfficeService.js
@@ -8,14 +8,30 @@
  * Service in the frontendApp.
  */
 angular.module('frontendApp')
-  .service('Office', function ($http, API_URL) {
+  .service('Office', function ($http, API_URL, OfficeModel, toastr) {
 
     function getOffices() {
-      return $http.get(API_URL + 'offices');
+      return $http.get(API_URL + 'offices')
+        .then(function (response) {
+          var results = [];
+          angular.forEach(response.data, function (office) {
+            results.push( new OfficeModel(office) );
+          });
+          return results;
+        })
+        .catch(function (response) {
+          toastr.error(response.statusText, 'Could not download offices.');
+        });
     }
 
     function getOffice(id) {
-      return $http.get(API_URL + 'offices/' + id);
+      return $http.get(API_URL + 'offices/' + id)
+        .then(function (response) {
+          return new OfficeModel(response.data);
+        })
+        .catch(function (response) {
+          toastr.error(response.statusText, 'Could not download office.');
+        });
     }
 
     return {
