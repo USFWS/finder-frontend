3eb325cd664984a6ecac18e27f40611434409c4c
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:49:09 -0400

    Ability to view office details and associated species

---
+++ app/offices/officeDetailCtrl.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name frontendApp.controller:OfficeDetailCtrl
+   * @description
+   * # ProfileCtrl
+   * Controller of the frontendApp
+   */
+  angular.module('frontendApp')
+    .controller('OfficeDetailCtrl', function ($scope, theOffice, $state) {
+      $scope.office = theOffice;
+
+      $scope.destroy = function (office) {
+        office.destroy();
+        $state.go('office.list');
+      };
+    });
+
+})();
