627a1c82b2f226bcd88626c8955306c407ec7a98
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:50:45 -0400

    Directive to manage office list when creating a new species

---
+++ app/species/updateSpeciesOfficeListDirective.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular.module('frontendApp')
+    .directive('updateSpeciesOfficeList', function () {
+      return {
+        restrict: 'E',
+        templateUrl: '/species/updateSpeciesOfficeList.html',
+        scope: {
+          species: '=',
+          offices: '='
+        },
+        controller: function ($scope) {
+
+          $scope.associateOffice = function (species, office) {
+            species.associateOffice(office)
+              .then(function (species) {
+                $scope.species = species;
+              });
+          };
+
+          $scope.removeAssociatedOffice = function (species, office) {
+            species.removeAssociatedOffice(office)
+              .then(function (species) {
+                $scope.species = species;
+              });
+          };
+        }
+      };
+    });
+})();
