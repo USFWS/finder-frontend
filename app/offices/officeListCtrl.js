67fbb25a673e76c07272c32303330775919298e1
Roy Hewitt <roy_hewitt@fws.gov>
2016-04-14 07:50:06 -0400

    Ability to view, sort, filter list of offices

---
+++ app/offices/officeListCtrl.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name frontendApp.controller:OfficeListCtrl
+   * @description
+   * # ProfileCtrl
+   * Controller of the frontendApp
+   */
+  angular.module('frontendApp')
+    .controller('OfficeListCtrl', function ($scope, officeList, User) {
+      $scope.offices = officeList;
+
+      $scope.isAdmin = function() {
+        return User.isAdmin();
+      };
+
+      $scope.isEditor = function() {
+        return User.isEditor();
+      };
+
+      $scope.remove = function (office) {
+        var index = $scope.offices.indexOf(office);
+        $scope.offices.splice(index, 1);
+        office.destroy();
+      };
+    });
+
+})();
