927bbe9f31aed2fda809629520472bc6e9e1472b
Roy Hewitt <roy_hewitt@fws.gov>
2019-07-16 15:07:15 -0400

    Display date for last status update in query results table

--- app/species/SpeciesModel.js
+++ app/species/SpeciesModel.js
@@ -9,211 +9,222 @@
    * Service in the frontendApp.
    */
 
-   angular.module('frontendApp')
-     .factory('SpeciesModel', ['$http', 'API_URL', 'toastr', function ($http, API_URL, toastr) {
-        var SpeciesModel = function (data) {
-          this.id = data.id;
-          this.itis = data.itis;
-          this.scientificName = data.scientificName;
-          this.commonName = data.commonName;
-          this.taxon = data.taxon;
-          this.offices = data.offices || [];
-          this.range = data.range || [];
-          this.status = data.status || [{}];
-          this.categorization = data.categorization || [];
-          this.proposedDetermination = data.proposedDetermination;
-          this.experts = data.experts || [];
-          this.lands = data.lands || [];
-          this.updatedAt = data.updatedAt;
-          this.ssa = data.ssa;
-        };
+  angular.module('frontendApp')
+    .factory('SpeciesModel', ['$http', 'API_URL', 'toastr', function ($http, API_URL, toastr) {
+      var SpeciesModel = function (data) {
+        this.id = data.id;
+        this.itis = data.itis;
+        this.scientificName = data.scientificName;
+        this.commonName = data.commonName;
+        this.taxon = data.taxon;
+        this.offices = data.offices || [];
+        this.range = data.range || [];
+        this.status = data.status || [{}];
+        this.categorization = data.categorization || [];
+        this.proposedDetermination = data.proposedDetermination;
+        this.experts = data.experts || [];
+        this.lands = data.lands || [];
+        this.updatedAt = data.updatedAt;
+        this.ssa = data.ssa;
+      };
 
-        SpeciesModel.prototype.create = function () {
-          return $http.post(API_URL + 'species', this)
-            .then(function (response) {
-              toastr.success('Successfully created ' + response.data.scientificName + '.');
-              return response;
-            })
-            .catch(function (response) {
-              var details = [];
-              angular.forEach(response.data.invalidAttributes, function (invalid) {
-                this.push(invalid[0].message);
-              }, details);
-              toastr.error(details.join('. '), response.statusText);
-            });
-        };
-
-        SpeciesModel.prototype.update = function () {
-          delete this.leadOffice;
-          return $http.put(API_URL + 'species/' + this.id, this)
-            .then(function (response) {
-              toastr.success('Successfully updated ' + response.data.scientificName + '.');
-            })
-            .catch(function (response) {
-              var details = [];
-              angular.forEach(response.data.invalidAttributes, function (invalid) {
-                this.push(invalid[0].message);
-              }, details);
-              toastr.error(details.join('. '), response.statusText);
-            });
-        };
-
-        SpeciesModel.prototype.destroy = function () {
-          var species = this;
-          return $http.delete(API_URL + 'species/' + this.id)
-            .then(function () {
-              toastr.success('Successfully removed ' + species.scientificName + ' from the database.');
-            })
-            .catch(function (response) {
-              var details = [];
-              angular.forEach(response.data.invalidAttributes, function (invalid) {
-                this.push(invalid[0].message);
-              }, details);
-              toastr.error(details.join('. '), response.statusText);
-            });
-        };
-
-        SpeciesModel.prototype.associateOffice = function (office) {
-          var self = this;
-          return $http.post(API_URL + 'species/' + self.id + '/offices/' + office.id)
-            .then(function (response) {
-              toastr.success('Successfully associated ' + self.scientificName + ' with ' + office.name + '.');
-              return new SpeciesModel(response.data);
-            })
-            .catch(function (response) {
-              toastr.error(response.statusText, 'Could not associate ' + self.scientificName + ' with ' + office.name + '.');
-              return response;
-            });
-        };
-
-        SpeciesModel.prototype.removeAssociatedOffice = function (office) {
-          var self = this;
-          return $http.delete(API_URL + 'species/' + self.id + '/offices/' + office.id)
-            .then(function (response) {
-              toastr.success('Successfully removed association between ' + self.scientificName + ' and ' + office.name + '.');
-              return new SpeciesModel(response.data);
-            })
-            .catch(function (response) {
-              toastr.error(response.statusText, 'Could not remove associattion between ' + self.scientificName + ' and ' + office.name + '.');
-              return response;
-            });
-        };
-
-        SpeciesModel.prototype.associateExpert = function (expert) {
-          var self = this;
-          return $http.post(API_URL + 'species/' + self.id + '/experts/' + expert.id)
-            .then(function (response) {
-              toastr.success('Successfully associated ' + self.scientificName + ' with ' + expert.name + '.');
-              return new SpeciesModel(response.data);
-            })
-            .catch(function (response) {
-              toastr.error(response.statusText, 'Could not associate ' + self.scientificName + ' with ' + expert.name + '.');
-              return response;
-            });
-        };
-
-        SpeciesModel.prototype.removeAssociatedExpert = function (expert) {
-          var self = this;
-          return $http.delete(API_URL + 'species/' + self.id + '/experts/' + expert.id)
-            .then(function (response) {
-              toastr.success('Successfully removed association between ' + self.scientificName + ' and ' + expert.name + '.');
-              return new SpeciesModel(response.data);
-            })
-            .catch(function (response) {
-              toastr.error(response.statusText, 'Could not remove associattion between ' + self.scientificName + ' and ' + expert.name + '.');
-              return response;
-            });
-        };
-
-        SpeciesModel.prototype.fullName = function () {
-          return this.commonName + ' (' + this.scientificName + ')';
-        };
-
-        SpeciesModel.prototype.currentStatus = function () {
-          var self = this;
-          var currentStatus = self.status[0];
-          angular.forEach(self.status, function(status) {
-            if ( new Date(status.date) > new Date(currentStatus.date) ) {
-              currentStatus = status;
-            }
+      SpeciesModel.prototype.create = function () {
+        return $http.post(API_URL + 'species', this)
+          .then(function (response) {
+            toastr.success('Successfully created ' + response.data.scientificName + '.');
+            return response;
+          })
+          .catch(function (response) {
+            var details = [];
+            angular.forEach(response.data.invalidAttributes, function (invalid) {
+              this.push(invalid[0].message);
+            }, details);
+            toastr.error(details.join('. '), response.statusText);
           });
-          return currentStatus.name;
-        };
+      };
 
-        SpeciesModel.prototype.removeEmptyStatus = function () {
-          var self = this;
-          angular.forEach(self.status, function (status, i) {
-            if ( !status.name && !status.date) {
-              self.status.splice(i, 1);
-            }
+      SpeciesModel.prototype.update = function () {
+        delete this.leadOffice;
+        return $http.put(API_URL + 'species/' + this.id, this)
+          .then(function (response) {
+            toastr.success('Successfully updated ' + response.data.scientificName + '.');
+          })
+          .catch(function (response) {
+            var details = [];
+            angular.forEach(response.data.invalidAttributes, function (invalid) {
+              this.push(invalid[0].message);
+            }, details);
+            toastr.error(details.join('. '), response.statusText);
           });
-        };
+      };
 
-        SpeciesModel.prototype.getLeadOfficeId = function () {
-          if ( this.office.length === 1 ) return this.office[0].id;
-          else return this.mostRecentOffice().id;
-        };
+      SpeciesModel.prototype.destroy = function () {
+        var species = this;
+        return $http.delete(API_URL + 'species/' + this.id)
+          .then(function () {
+            toastr.success('Successfully removed ' + species.scientificName + ' from the database.');
+          })
+          .catch(function (response) {
+            var details = [];
+            angular.forEach(response.data.invalidAttributes, function (invalid) {
+              this.push(invalid[0].message);
+            }, details);
+            toastr.error(details.join('. '), response.statusText);
+          });
+      };
 
-        SpeciesModel.prototype.getLeadOfficeName = function () {
-          var offices = [];
-          if ( this.offices.length === 0 ) return 'No lead office specified';
-          if ( this.offices.length === 1 ) return this.offices[0].name;
-          else {
-            angular.forEach(this.offices, function (office) {
-              offices.push(office.name);
-            });
-            return offices.join(', ');
+      SpeciesModel.prototype.associateOffice = function (office) {
+        var self = this;
+        return $http.post(API_URL + 'species/' + self.id + '/offices/' + office.id)
+          .then(function (response) {
+            toastr.success('Successfully associated ' + self.scientificName + ' with ' + office.name + '.');
+            return new SpeciesModel(response.data);
+          })
+          .catch(function (response) {
+            toastr.error(response.statusText, 'Could not associate ' + self.scientificName + ' with ' + office.name + '.');
+            return response;
+          });
+      };
+
+      SpeciesModel.prototype.removeAssociatedOffice = function (office) {
+        var self = this;
+        return $http.delete(API_URL + 'species/' + self.id + '/offices/' + office.id)
+          .then(function (response) {
+            toastr.success('Successfully removed association between ' + self.scientificName + ' and ' + office.name + '.');
+            return new SpeciesModel(response.data);
+          })
+          .catch(function (response) {
+            toastr.error(response.statusText, 'Could not remove associattion between ' + self.scientificName + ' and ' + office.name + '.');
+            return response;
+          });
+      };
+
+      SpeciesModel.prototype.associateExpert = function (expert) {
+        var self = this;
+        return $http.post(API_URL + 'species/' + self.id + '/experts/' + expert.id)
+          .then(function (response) {
+            toastr.success('Successfully associated ' + self.scientificName + ' with ' + expert.name + '.');
+            return new SpeciesModel(response.data);
+          })
+          .catch(function (response) {
+            toastr.error(response.statusText, 'Could not associate ' + self.scientificName + ' with ' + expert.name + '.');
+            return response;
+          });
+      };
+
+      SpeciesModel.prototype.removeAssociatedExpert = function (expert) {
+        var self = this;
+        return $http.delete(API_URL + 'species/' + self.id + '/experts/' + expert.id)
+          .then(function (response) {
+            toastr.success('Successfully removed association between ' + self.scientificName + ' and ' + expert.name + '.');
+            return new SpeciesModel(response.data);
+          })
+          .catch(function (response) {
+            toastr.error(response.statusText, 'Could not remove associattion between ' + self.scientificName + ' and ' + expert.name + '.');
+            return response;
+          });
+      };
+
+      SpeciesModel.prototype.fullName = function () {
+        return this.commonName + ' (' + this.scientificName + ')';
+      };
+
+      SpeciesModel.prototype.currentStatus = function () {
+        var self = this;
+        var currentStatus = self.status[0];
+        angular.forEach(self.status, function (status) {
+          if (new Date(status.date) > new Date(currentStatus.date)) {
+            currentStatus = status;
           }
-        };
+        });
+        return currentStatus.name;
+      };
 
-        SpeciesModel.prototype.mostRecentOffice = function () {
-          var mostRecent = this.office[0];
-          angular.forEach(this.office, function (office) {
-            if (new Date(mostRecent.updatedAt) < new Date(office.updatedAt) )
-              mostRecent = office;
-          });
-          return mostRecent;
-        };
-
-        SpeciesModel.prototype.lastCategoryAsText = function () {
-          var text = '';
-          var category = this.categorization[this.categorization.length -1];
-          if ( category === undefined ) return text;
-          else text = [category.code, category.name].join(' - ');
-          return text;
-        };
-
-        SpeciesModel.prototype.addCategory = function (category) {
-          this.categorization.push(category);
-          return this.categorization;
-        };
-
-        SpeciesModel.prototype.removeCategory = function (category) {
-          var index = this.categorization.indexOf(category);
-          if (index > -1 ) this.categorization.splice(index, 1);
-          return this.categorization;
-        };
-
-        SpeciesModel.prototype.validate = function () {
-          var self = this;
-          var lastStatus = self.status[self.status.length -1];
-          if (self.range.length === 0) {
-            toastr.error('You must specify at least one state for this specie\'s range.');
-            return false;
-          } else if (self.status.length === 1 && lastStatus.name === undefined && lastStatus.date === undefined) {
-            toastr.error('You must specify at least one status to create a species.');
-            return false;
-          } else if (self.status.length > 1 && lastStatus.name === undefined && lastStatus.date === undefined) {
-            self.status.pop();
+      SpeciesModel.prototype.currentStatusDate = function () {
+        var self = this;
+        var currentStatus = self.status[0];
+        angular.forEach(self.status, function (status) {
+          if (new Date(status.date) > new Date(currentStatus.date)) {
+            currentStatus = status;
           }
-          return true;
-        };
+        });
+        return currentStatus.date;
+      };
 
-        SpeciesModel.prototype.alphabetizeRange = function () {
-          return this.range.sort();
-        };
+      SpeciesModel.prototype.removeEmptyStatus = function () {
+        var self = this;
+        angular.forEach(self.status, function (status, i) {
+          if (!status.name && !status.date) {
+            self.status.splice(i, 1);
+          }
+        });
+      };
 
-        return SpeciesModel;
-      }]);
+      SpeciesModel.prototype.getLeadOfficeId = function () {
+        if (this.office.length === 1) return this.office[0].id;
+        else return this.mostRecentOffice().id;
+      };
+
+      SpeciesModel.prototype.getLeadOfficeName = function () {
+        var offices = [];
+        if (this.offices.length === 0) return 'No lead office specified';
+        if (this.offices.length === 1) return this.offices[0].name;
+        else {
+          angular.forEach(this.offices, function (office) {
+            offices.push(office.name);
+          });
+          return offices.join(', ');
+        }
+      };
+
+      SpeciesModel.prototype.mostRecentOffice = function () {
+        var mostRecent = this.office[0];
+        angular.forEach(this.office, function (office) {
+          if (new Date(mostRecent.updatedAt) < new Date(office.updatedAt))
+            mostRecent = office;
+        });
+        return mostRecent;
+      };
+
+      SpeciesModel.prototype.lastCategoryAsText = function () {
+        var text = '';
+        var category = this.categorization[this.categorization.length - 1];
+        if (category === undefined) return text;
+        else text = [category.code, category.name].join(' - ');
+        return text;
+      };
+
+      SpeciesModel.prototype.addCategory = function (category) {
+        this.categorization.push(category);
+        return this.categorization;
+      };
+
+      SpeciesModel.prototype.removeCategory = function (category) {
+        var index = this.categorization.indexOf(category);
+        if (index > -1) this.categorization.splice(index, 1);
+        return this.categorization;
+      };
+
+      SpeciesModel.prototype.validate = function () {
+        var self = this;
+        var lastStatus = self.status[self.status.length - 1];
+        if (self.range.length === 0) {
+          toastr.error('You must specify at least one state for this specie\'s range.');
+          return false;
+        } else if (self.status.length === 1 && lastStatus.name === undefined && lastStatus.date === undefined) {
+          toastr.error('You must specify at least one status to create a species.');
+          return false;
+        } else if (self.status.length > 1 && lastStatus.name === undefined && lastStatus.date === undefined) {
+          self.status.pop();
+        }
+        return true;
+      };
+
+      SpeciesModel.prototype.alphabetizeRange = function () {
+        return this.range.sort();
+      };
+
+      return SpeciesModel;
+    }]);
 
 })();
